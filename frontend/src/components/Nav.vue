9036a3517e364bd004bc9ac1ef77db4b32e0cf9f
Adrian Pasterka <pasterkaadrian@gmail.com>
2024-04-29 14:13:57 +0200

    Add animation to config

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -23,7 +23,7 @@ const items = ref([{
 
 <template>
 
-    <MegaMenu :model="items" breakpoint="1023px"
+    <MegaMenu :model="items" breakpoint="1023px" class=" animate-puff-in-center"
         :pt="{ root: { class: 'flex justify-between p-4 bg-[#485262]' }, menu: { class: 'max-lg:bg-red-400 grow' }, menubutton: { class: 'bg-red-400' }, menuitem: { class: 'flex-1 h-16 hover:bg-red-500 bg-red-400 flex items-center justify-left md:justify-center cursor-pointer mx-6 rounded-lg' } }">
         <template #start>
             <h2 class=" font-greatvibes font-light text-[2rem] text-red-400">bus plan</h2>
